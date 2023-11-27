7bc8d712a243fd0a88ea38474e85f5f782013b3b
katrinkan <96773609+katrinkan@users.noreply.github.com>
2023-11-27 11:29:31 +0100

    added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-function handleSubmit(event) {
-  event.preventDefault();
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "Nice to meet you, where've you been",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
+async function generatePoem(event) {
+  event.preventDefault();
+  let userInput = document.querySelector("#theme");
+  let poemElement = document.querySelector("#poem");
+  const apiKey = "cc10t459o5bb3a3939c42b9f760a834f";
+  const context =
+    "You are an AI assistant writing short poems. Your mission is to generate a 4 line poem in basic HTML using <p/> and seperate each line using <br/>";
+  const prompt = `Generate a poem about ${userInput.value}`;
+  const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  poemElement.innerHTML = `Generating a poem about ${userInput.value.toLowerCase()}`;
+  try {
+    const response = await axios.get(apiUrl);
+    console.log(response);
+    displayPoem(response);
+  } catch (error) {
+    poemElement.innerHTML = "Sorry, something went wrong. Please try again!";
+    console.log(error);
+  }
+}
+
 let themeInputElement = document.querySelector("#theme-input");
-themeInputElement.addEventListener("submit", handleSubmit);
+themeInputElement.addEventListener("submit", generatePoem);
