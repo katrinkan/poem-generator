4abb833a011484959166dbff58e1d4008bf5cf45
katrinkan <96773609+katrinkan@users.noreply.github.com>
2023-11-27 10:39:50 +0100

    added form handling and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+function handleSubmit(event) {
+  event.preventDefault();
+  new Typewriter("#poem", {
+    strings: "Nice to meet you, where've you been",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let themeInputElement = document.querySelector("#theme-input");
+themeInputElement.addEventListener("submit", handleSubmit);
