0c300c7d9df572cfacd1446192790f11c7d5c16d
katrinkan <96773609+katrinkan@users.noreply.github.com>
2023-12-01 09:55:58 +0100

    changed link color for better contrast

--- src/index.js
+++ src/index.js
@@ -29,19 +29,16 @@ function displayPoem(response) {
 
 async function generatePoem(event) {
   event.preventDefault();
-
   const apiKey = "cc10t459o5bb3a3939c42b9f760a834f";
   const context =
     "You are an AI assistant writing short poems. Your mission is to generate a 4 line poem in basic HTML using <p/> and seperate each line using <br/>";
   const prompt = `Generate a poem about ${userInput.value}`;
   const apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
   poemElement.classList.remove("hidden");
   poemElement.innerHTML = `Generating a poem about ${userInput.value.toLowerCase()}`;
 
   try {
     const response = await axios.get(apiUrl);
-    console.log(response);
     displayPoem(response);
   } catch (error) {
     poemElement.innerHTML = "Sorry, something went wrong. Please try again!";
